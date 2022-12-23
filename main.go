bbf7ca7178f1734c97019592e366b91314a561de
yandi527 <yandi.lin@thoughtworks.com>
2022-12-23 09:02:47 +0800

    [yandi] refactor: move root cmd to cmd directory

--- main.go
+++ main.go
@@ -1,18 +1,17 @@
 package main
 
-import (
-	"fmt"
-	"github.com/spf13/cobra"
-)
+import "testrunner/cmd"
 
 func main() {
-	command := cobra.Command{
-		Args: cobra.ExactArgs(0),
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("yes")
-		},
-	}
-	if err := command.Execute(); err != nil {
-		panic(err)
-	}
+	//command := cobra.Command{
+	//	Args: cobra.ExactArgs(0),
+	//	Run: func(cmd *cobra.Command, args []string) {
+	//		fmt.Println("yes")
+	//		testrunner := test_runner.Testrunner{}
+	//		testrunner.Run("/Users/yandi.lin/Documents/test_mesh/testrunner-demo/test_runner/unit_test/data/testsuite_correct_baidu_request")
+	//		report := test_report.GetReport()
+	//		report.Gen("./gen")
+	//	},
+	//}
+	cmd.Execute()
 }
