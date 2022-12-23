bda0d29a4b790aac8e016ae27b3fdeb0ae024f3e
yandi527 <yandi.lin@thoughtworks.com>
2022-12-23 09:17:04 +0800

    [yandi] feat: change field type in assertion class

--- test_suite/api_stage.go
+++ test_suite/api_stage.go
@@ -1,7 +1,6 @@
 package test_suite
 
 import (
-	"io"
 	"net/http"
 	"testrunner/common"
 	"testrunner/test_assertion"
@@ -17,7 +16,7 @@ type Request struct {
 }
 
 type Assertion struct {
-	Status  string            `yaml:"status,omitempty"`
+	Status  int               `yaml:"status,omitempty"`
 	Headers map[string]string `yaml:"headers,omitempty"`
 	Body    struct {
 		Equals string `yaml:"equals,omitempty"`
@@ -25,7 +24,6 @@ type Assertion struct {
 }
 
 func (s *Stage) executeApi() error {
-
 	if common.CurrentPhase() == common.Asserting {
 		//send http request base on stage request
 		request, err := http.NewRequest(s.Request.Method, s.Request.Url, nil)
@@ -41,25 +39,15 @@ func (s *Stage) executeApi() error {
 		statusCode := res.StatusCode
 		//get body from response
 		body := res.Body
-		resByte, err := io.ReadAll(body)
 		if err != nil {
 			panic(err)
 		}
-		resString := string(resByte)
+
 		defer body.Close()
-		//compare with expect and generate pass
-		//pass := (s.Expect.Res == resString) && (s.Expect.Status == statusCode)
-		pass := s.Expect.Status == statusCode
-		//generate compare
-		compare := Compare{expect: s.Expect, actual: Response{statusCode, resString}}
-		//return result
-		result := Result{s.Request, Response{statusCode, resString}, compare, pass}
-		results := append(report.Results, result)
-		report.Results = results[:]
 
 		assertor := test_assertion.GenericAssertor{}
 
-		assert, err := assertor.Assert(s.Assertion.Status, test_assertion.Equals, s.Assertion.Status)
+		assert, err := assertor.Assert(statusCode, test_assertion.Equals, s.Assertion.Status)
 		if err != nil || !assert {
 			s.apiReport(assert, err.Error())
 		} else {
