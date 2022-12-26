9a52f0ad474bc5e7335990e978bbb39a1e81066a
yandi527 <yandi.lin@thoughtworks.com>
2022-12-26 09:21:32 +0800

    [yandi] refactor: delete useless comments in command

--- cmd/exec.go
+++ cmd/exec.go
@@ -7,21 +7,14 @@ import (
 )
 
 var execCmd = &cobra.Command{
-	Use:   "exec",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use: "exec",
 	Run: func(cmd *cobra.Command, args []string) {
 		testrunner := test_runner.Testrunner{}
 		err := testrunner.Run(args[0])
 		if err != nil {
 			panic(err)
 		}
-		_, err = test_report.GetReport().Gen("./gen")
+		_, err = test_report.GetReport().Gen(".")
 		if err != nil {
 			panic(err)
 		}
@@ -30,14 +23,4 @@ to quickly create a Cobra application.`,
 
 func init() {
 	rootCmd.AddCommand(execCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// testCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// testCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
